02dc30d1c4ec081a1cdad5972f36537330b0bd1f
Kristi Uukkivi <kristi.uukkivi@sk.ee>
2015-10-15 11:13:07 +0300

    Added v3.12 API changes doc

---
+++ v3.12/classdigidoc_1_1DataFile.js
@@ -0,0 +1,14 @@
+var classdigidoc_1_1DataFile =
+[
+    [ "calcDigest", "classdigidoc_1_1DataFile.html#a6da4a7e400df9624a58a9b183d8e6c42", null ],
+    [ "fileName", "classdigidoc_1_1DataFile.html#a903e6871c992c18c691d0b325e2912e5", null ],
+    [ "fileSize", "classdigidoc_1_1DataFile.html#a3b124446bfd7d53fca011dcbe6ec032f", null ],
+    [ "id", "classdigidoc_1_1DataFile.html#a579f1fcb800ee16759dae16c628392fd", null ],
+    [ "mediaType", "classdigidoc_1_1DataFile.html#a4094bfb5a6042d74d6b854d0367d6d46", null ],
+    [ "saveAs", "classdigidoc_1_1DataFile.html#a300e578dc9e23dbf0bf5b2ee0ef55e15", null ],
+    [ "saveAs", "classdigidoc_1_1DataFile.html#a2c7b57670721be8457afcd8cd67ebca8", null ],
+    [ "BDoc", "classdigidoc_1_1DataFile.html#a346045e333099e698ea9e36376773f07", null ],
+    [ "DDoc", "classdigidoc_1_1DataFile.html#a8b0e649e20f3d7126551ab93e9f924f2", null ],
+    [ "SignatureA", "classdigidoc_1_1DataFile.html#a8268d22e3a2434f70dd45a7590e7c1c6", null ],
+    [ "URIResolver", "classdigidoc_1_1DataFile.html#a73a7ec234893347c771831d2208044a1", null ]
+];
---
+++ v3.12/classdigidoc_1_1PKCS12Signer.js
@@ -0,0 +1,5 @@
+var classdigidoc_1_1PKCS12Signer =
+[
+    [ "PKCS12Signer", "classdigidoc_1_1PKCS12Signer.html#aeb4abb31b32d014282eb5a8287102f86", null ],
+    [ "~PKCS12Signer", "classdigidoc_1_1PKCS12Signer.html#aae592ee26668262da8c03bb4849b7a36", null ]
+];
